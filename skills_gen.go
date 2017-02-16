d3b8a93be2056837c1d34fb183347e0ca2a44c53
Max Hunt <max@huntw3.com>
2017-02-16 22:44:13 +0000

    Improve go coding style.

--- skills_gen.go
+++ skills_gen.go
@@ -1,41 +1,58 @@
 package main
 
-import "text/template"
-import "encoding/json"
-import "os"
-import "strings"
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"text/template"
+)
 
 type skill struct {
-	Name        string `json:name`
-	Code_name   string
+	Name        string  `json:name`
 	Cost        int     `json:cost`
 	Description string  `json:decription`
 	Passive     bool    `json:passive`
 	Subskills   []skill `json:subskills`
+
+	CodeName string
 }
 
-func generate_code_names(skills []skill) {
+// We need a code name for each skill, for use as html id and JS variable.
+// Generate these by lowercasing and replacing spaces with underscores.
+// "My Cool Skill" -> "my_cool_skill"
+func generateCodeNames(skills []skill) {
 	for i := range skills {
-		skills[i].Code_name = strings.Replace(strings.ToLower(skills[i].Name), " ", "_", -1)
-		generate_code_names(skills[i].Subskills)
+		skills[i].CodeName = strings.Replace(strings.ToLower(skills[i].Name), " ", "_", -1)
+		generateCodeNames(skills[i].Subskills)
 	}
 }
 
 func main() {
+
 	tmpl := template.Must(template.ParseFiles("skills.html.template"))
-	file, _ := os.Open("sword_skills.json")
-	dec := json.NewDecoder(file)
-	var skills []skill
-	err := dec.Decode(&skills)
+
+	file, err := os.Open("sword_skills.json")
 	if err != nil {
 		panic(err)
 	}
-	generate_code_names(skills)
+
+	dec := json.NewDecoder(file)
+
+	var skills []skill
+	err = dec.Decode(&skills)
+	if err != nil {
+		panic(err)
+	}
+
+	generateCodeNames(skills)
+
 	fmt.Println("<ul>")
+
 	err = tmpl.Execute(os.Stdout, skills[0])
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("</ul>")
 }
